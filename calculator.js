2c77449f2e41870737269ef18c531981796eec6e
TLandWeb <tlandisdesign@gmail.com>
2023-03-30 17:39:59 -0400

    Add ability for buttons to change the input number

--- calculator.js
+++ calculator.js
@@ -33,3 +33,37 @@ function operate(a, b, op) {
 let firstNumber;
 let secondNumber;
 let operator;
+
+const btnOne = document.querySelector('#num1');
+const numberFrame = document.querySelector('#numberFrame');
+document.getElementById('num1').addEventListener('click', function() {
+    numberFrame.value = numberFrame.value + "1";
+});
+document.getElementById('num2').addEventListener('click', function() {
+    numberFrame.value = numberFrame.value + "2";
+});
+document.getElementById('num3').addEventListener('click', function() {
+    numberFrame.value = numberFrame.value + "3";
+});
+document.getElementById('num4').addEventListener('click', function() {
+    numberFrame.value = numberFrame.value + "4";
+});
+document.getElementById('num5').addEventListener('click', function() {
+    numberFrame.value = numberFrame.value + "5";
+});
+document.getElementById('num6').addEventListener('click', function() {
+    numberFrame.value = numberFrame.value + "6";
+});
+document.getElementById('num7').addEventListener('click', function() {
+    numberFrame.value = numberFrame.value + "7";
+});
+document.getElementById('num8').addEventListener('click', function() {
+    numberFrame.value = numberFrame.value + "8";
+});
+document.getElementById('num9').addEventListener('click', function() {
+    numberFrame.value = numberFrame.value + "9";
+});
+document.getElementById('num0').addEventListener('click', function() {
+    numberFrame.value = numberFrame.value + "0";
+});
+
